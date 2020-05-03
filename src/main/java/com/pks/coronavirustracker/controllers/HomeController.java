20ed0dbd8af8b4ebe2a2fa4213e1b3a0046fe9be
PKS <pkumarsaha71@gmail.com>
2020-05-03 19:32:17 +0600

    0.1

--- src/main/java/com/pks/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/pks/coronavirustracker/controllers/HomeController.java
@@ -2,11 +2,18 @@ package com.pks.coronavirustracker.controllers;
 
 //controller helps to show the URLS to have UI like use HTML
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    //put things in the model and acess it here and show it in the home.html
 
-    public String home(){
+    @GetMapping("/")
+    public String home(Model model){
+        //mnow this model is accesable in home.html
+        model.addAttribute("TestName","NAME");
 
+        return "home";
     }
 }
