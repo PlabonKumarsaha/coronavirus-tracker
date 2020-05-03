8ce8041ac9aa334eac18f70be3ee0e72b07f7df1
PKS <pkumarsaha71@gmail.com>
2020-05-03 19:22:50 +0600

    0.1

--- src/main/java/com/pks/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/pks/coronavirustracker/controllers/HomeController.java
@@ -1,8 +1,12 @@
 package com.pks.coronavirustracker.controllers;
 
-//controller helps to show the URLS to have UI like use HTML 
+//controller helps to show the URLS to have UI like use HTML
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class HomeController {
+
+    public String home(){
+
+    }
 }
